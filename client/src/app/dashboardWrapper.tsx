cd161c92d3a0eef2428111c582b2434401fad7ff
adilmusali <adil.musali.1602@gmail.com>
2025-03-09 21:50:48 +0200

    fix: adjust the code for fixing navbar

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -2,10 +2,10 @@
 
 import React, { useEffect } from "react";
 import Navbar from "@/app/(components)/Navbar";
-import Sidebar from "@/app/(components)/Sidebar"
+import Sidebar from "@/app/(components)/Sidebar";
 import StoreProvider, { useAppSelector } from "./redux";
 
-function DashboardLayout({ children }: { children: React.ReactNode }) {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
     const isSidebarCollapsed = useAppSelector(
         (state) => state.global.isSidebarCollapsed
     );
@@ -22,25 +22,29 @@ function DashboardLayout({ children }: { children: React.ReactNode }) {
 
     return (
         <div
-            className={`${isDarkMode ? "dark" : "light"} flex bg-gray-50 text-gray-900 w-full min-h-screen`}
+            className={`${
+                isDarkMode ? "dark" : "light"
+            } flex bg-gray-50 text-gray-900 w-full min-h-screen`}
         >
             <Sidebar />
             <main
-                className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${isSidebarCollapsed} ? "md:pl-24" : "md:pl-72`}
+                className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${
+                    isSidebarCollapsed ? "md:pl-24" : "md:pl-72"
+                }`}
             >
                 <Navbar />
                 {children}
             </main>
         </div>
     );
-}
+};
 
-function DashboardWrapper({ children }: { children: React.ReactNode }) {
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
     return (
         <StoreProvider>
             <DashboardLayout>{children}</DashboardLayout>
         </StoreProvider>
     );
-}
+};
 
 export default DashboardWrapper;
