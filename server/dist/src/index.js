1bbaff4fb35c70823456bf374791036fba7bd948
adilmusali <adil.musali.1602@gmail.com>
2025-03-09 22:06:03 +0200

    feat(dashboard components): Implement CardPopularProducts component to display popular products
    - Implement CardPurchaseSummary component to display purchase summary
    
    - Implement CardSalesSummary component to display sales summary

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const body_parser_1 = __importDefault(require("body-parser"));
+const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
 const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
@@ -19,6 +20,7 @@ app.use(helmet_1.default.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use((0, morgan_1.default)("common"));
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use((0, cors_1.default)());
 //ROUTES
 app.use("/dashboard", dashboardRoutes_1.default);
 //SERVER
