4e964b91acfb3d9c470f2929fd74ac8bcbe10a66
adilmusali <adil.musali.1602@gmail.com>
2025-03-12 03:38:56 +0200

    Configured Next.js to allow remote images from S3 bucket

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "myinventorymanagements3.s3.eu-north-1.amazonaws.com",
+                port: "",
+                pathname: "/**"
+            }
+        ]
+    }
+};
 
 export default nextConfig;
